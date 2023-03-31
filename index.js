1451b75e0ed88a2b6fdb8e423b3748ff4e1fcb04
Venus5959 <109614730+Venus5959@users.noreply.github.com>
2023-03-31 10:05:48 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,71 @@
+/*capitulo 1 */
+function agregarClase() {
+    const puerta1 = document.getElementById('vibrar');
+    puerta1.classList.add('activar-vibrar');
+
+
+
+    setTimeout(() => {
+    puerta1.classList.remove('activar-vibrar');
+  }, 300);
+}
+ 
+
+function olim1() {
+  const tx1 = document.getElementById('ocultar');
+  tx1.classList.add('visible');
+}
+
+/*capitulo2 */
+
+function filipo2() {
+  const tx2 = document.getElementById('ocultar2');
+  tx2.classList.add('visible');
+}
+
+function olimpia2() {
+  const tx22 = document.getElementById('ocultar22');
+  tx22.classList.add('visible');
+}
+
+/*capitulo 3 */
+
+function alejandro3() {
+  const tx31 = document.getElementById('ocultar31');
+  tx31.classList.add('visible');
+}
+
+
+function alejandro32() {
+  const tx32 = document.getElementById('ocultar32');
+  tx32.classList.add('visible');
+}
+
+function alejandro33() {
+  const tx33 = document.getElementById('ocultar33');
+  tx33.classList.add('visible');
+}
+
+
+/*capitulo 4 */
+
+function fondo4() {
+  const tx4 = document.getElementById('ocultar4');
+  const tx41 = document.getElementById('ocultar41');
+  const tx42 = document.getElementById('ocultar42');
+  const alejo43 = document.getElementById('mover');
+  
+  tx4.classList.add('visible');
+  tx41.classList.add('visible');
+  tx42.classList.add('visible');
+  alejo43.classList.add('alejandro42-movimiento');
+}
+
+function alejandro42() {
+  const tx43 = document.getElementById('tx43');
+  const alejandro4 = document.getElementById('alejo4');
+  const alejandro42 = document.getElementById('mover');
+  tx43.classList.add('visible');
+  alejandro4.classList.add('visible');
+  alejandro42.classList.add('visible');
+}
---
+++ segundo.js
@@ -0,0 +1,104 @@
+
+
+  const pelea1 = document.getElementById('pelea1');
+    const pelea2 = document.getElementById('pelea2');
+    const pelea3 = document.getElementById('pelea3');
+    const pelea4 = document.getElementById('pelea4');
+    const polvo = document.getElementById('polvo');
+    const polvo2 = document.getElementById('polvo2');
+    const polvo3 = document.getElementById('polvo3');
+    const polvo4 = document.getElementById('polvo4');
+    const bling = document.getElementById('bling');
+
+pelea1.addEventListener('click', () => {
+  pelea1.style.animationPlayState = 'running'; 
+  pelea2.style.animationPlayState = 'running'; 
+  pelea3.style.animationPlayState = 'running'; 
+  pelea4.style.animationPlayState = 'running'; 
+  polvo.style.animationPlayState = 'running'; 
+  polvo2.style.animationPlayState = 'running'; 
+  polvo3.style.animationPlayState = 'running'; 
+  polvo4.style.animationPlayState = 'running'; 
+  bling.style.animationPlayState = 'running';
+});
+
+
+
+const fondo = document.getElementById('fondo5');
+const tropa1 = document.getElementById('tropa1');
+const tropa2 = document.getElementById('tropa2');
+const alejandro51= document.getElementById('alejandro51');
+const dario51 = document.getElementById('dario51');
+const tx53 = document.getElementById('tx53');
+const tx54 = document.getElementById('tx54');
+
+
+fondo.addEventListener('click', () => {
+  tropa1.style.animationPlayState = 'running'; 
+  tropa2.style.animationPlayState = 'running';
+  alejandro51.style.animationPlayState = 'running'; 
+  dario51.style.animationPlayState = 'running'; 
+  tx53.style.animationPlayState = 'running'; 
+  tx54.style.animationPlayState = 'running'; 
+}
+);
+
+
+const dario52 = document.getElementById('dario52');
+const polvo5 = document.getElementById('polvo5');
+const alejandro52 = document.getElementById('alejandro52');
+
+dario52.addEventListener('click', () => {
+  dario52.style.animationPlayState = 'running'; 
+  polvo5.style.animationPlayState = 'running';
+  alejandro52.style.animationPlayState = 'running';
+}
+);
+
+/*capitulo 6 */
+
+
+const caballo6 = document.getElementById('caballo6');
+
+caballo6.addEventListener('click', () => {
+  caballo6.style.animationPlayState = 'running'; 
+}
+);
+
+const alejandro6 = document.getElementById('alejandro6');
+
+alejandro6.addEventListener('click', () => {
+  alejandro6.style.animationPlayState = 'running'; 
+}
+);
+
+const moneda = document.getElementById('moneda');
+
+moneda.addEventListener('click', () => {
+  moneda.style.animationPlayState = 'running'; 
+}
+);
+
+
+/*capitulo 7 */
+const fondo7 = document.getElementById('fondo7');
+const texto7 = document.getElementById('texto7');
+
+
+fondo7.addEventListener('click', () => {
+  texto7.style.animationPlayState = 'running'; 
+}
+);
+
+
+const logo = document.getElementById('logo');
+const personajes = document.getElementById('personajes');
+const nube = document.getElementById('nube');
+
+logo.addEventListener('click', () => {
+  personajes.style.animationPlayState = 'running'; 
+  nube.style.animationPlayState = 'running'; 
+}
+);
+
+
